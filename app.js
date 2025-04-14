47fae8e9602ac46b57e14d179fbb4ebeea0b1ef2
User Name <landonisaiah@gmail.com>
2025-04-14 15:27:22 -0700

    fisnished through exercise 12

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
 const pokemon = require('./data.js');
 
 const game = {
-    party: [],
-    gyms: [
-      { location: "Pewter City", completed: false, difficulty: 1 },
-      { location: "Cerulean City", completed: false, difficulty: 2 },
-      { location: "Vermilion City", completed: false, difficulty: 3 },
-      { location: "Celadon City", completed: false, difficulty: 4 },
-      { location: "Fuchsia City", completed: false, difficulty: 5 },
-      { location: "Saffron City", completed: false, difficulty: 6 },
-      { location: "Cinnabar Island", completed: false, difficulty: 7 },
-      { location: "Viridian City", completed: false, difficulty: 8 },
-    ],
-    items: [
-      { name: "potion", quantity: 4 },
-      { name: "pokeball", quantity: 8 },
-      { name: "rare candy", quantity: 99 },
-    ],
-  }
+  party: [],
+  gyms: [
+    { location: "Pewter City", completed: false, difficulty: 1 },
+    { location: "Cerulean City", completed: false, difficulty: 2 },
+    { location: "Vermilion City", completed: false, difficulty: 3 },
+    { location: "Celadon City", completed: false, difficulty: 4 },
+    { location: "Fuchsia City", completed: false, difficulty: 5 },
+    { location: "Saffron City", completed: false, difficulty: 6 },
+    { location: "Cinnabar Island", completed: false, difficulty: 7 },
+    { location: "Viridian City", completed: false, difficulty: 8 },
+  ],
+  items: [
+    { name: "potion", quantity: 4 },
+    { name: "pokeball", quantity: 8 },
+    { name: "rare candy", quantity: 99 },
+  ],
+}
 
 // console.dir(pokemon, { maxArrayLength: null })
 // console.log(pokemon[58]);
@@ -145,7 +145,7 @@ Exercise 8
 Solve Exercise 8 here:
 */
 
-game.party.forEach (pokemon => {
+game.party.forEach(pokemon => {
   console.log(pokemon.name);
 });
 
@@ -178,12 +178,52 @@ After writing this method, call it and pass in a Pokemon object of your choice f
 Solve Exercise 10 here:
 */
 
-game.catchPokemon = function(pokemonObj) {
+game.catchPokemon = function (pokemonObj) {
   this.party.push(pokemonObj);
 };
 
-game.catchPokemon(pokemon[73]);
-console.log(game.party);
+// game.catchPokemon(pokemon[73]);
+// console.log(game.party);
+
+
+/*
+Exercise 11
+1. Copy the `catchPokemon` method that you just wrote above, and paste it below. Modify it so that it also decreases the number of pokeballs in your inventory each time you catch a Pokémon.
+2. How will you find and update the quantity of pokeballs in the `game.items` array?
+
+Tips:
+For this exercise, it's okay to have a negative number of pokeballs.
+After updating the method, call it and pass in a Pokemon object of your choice from the `pokemon` data to catch it.
+Also, log the `game.items` array to confirm that the pokeball quantity is being decremented.
+
+Solve Exercise 11 here:
+*/
+
+const pokeBall = game.items.find(item => item.name === "pokeball");
+
+game.catchPokemon = function (pokemonObj) {
+  this.party.push(pokemonObj);
+  pokeBall.quantity -=1;
+};
+
+game.catchPokemon(pokemon[21]);
+console.log(game);
+
+/*
+Exercise 12
+1. Similar to Exercise 6, now complete gyms with a difficulty below 6. How will you approach this?
+ (change the value of `complete` in the qualifying objects from false to true).
+
+Solve Exercise 12 here:
+*/
+
+game.gyms.forEach((gym) => {
+  if (gym.difficulty < 6) {
+    gym.completed = true;
+  }
+});
+
+console.log(game.gyms);
 
 
 
@@ -193,4 +233,3 @@ console.log(game.party);
 
 
 
-  
